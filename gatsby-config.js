a248e513574a00bc984a9d9c0d30f6d0c331797d
wonhyeong lee <bigfile57@naver.com>
2022-08-22 19:36:06 +0900

    배포설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `원형도서관`,
+    description: `개인기록용 블로그`,
+    author: `이원형`,
+    siteUrl: `https://WonhyeongLee.github.io/`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://WonhyeongLee.github.io/',
+        stripQueryString: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
         isTSX: true, // defaults to false
         jsxPragma: `jsx`, // defaults to "React"
         allExtensions: true, // defaults to false
-      }
+      },
     },
     `gatsby-plugin-image`,
     {
